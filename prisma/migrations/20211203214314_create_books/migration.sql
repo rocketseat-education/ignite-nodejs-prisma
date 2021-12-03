64438bc393932026dd94be8d8b4225e86c3adb5a
Daniele <danileaodev@gmail.com>
2021-12-03 18:47:27 -0300

    Result

---
+++ prisma/migrations/20211203214314_create_books/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "books" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "author_id" TEXT NOT NULL,
+
+    CONSTRAINT "books_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "books_name_key" ON "books"("name");
+
+-- AddForeignKey
+ALTER TABLE "books" ADD CONSTRAINT "books_author_id_fkey" FOREIGN KEY ("author_id") REFERENCES "authors"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
