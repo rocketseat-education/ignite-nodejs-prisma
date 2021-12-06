8eb243da83aee282d3a66937fa78ac8af4e7858f
Daniele <danileaodev@gmail.com>
2021-12-06 15:25:35 -0300

    Result

--- src/courses/create.ts
+++ src/courses/create.ts
@@ -5,9 +5,9 @@ const prisma = new PrismaClient();
 async function main() {
   const result = await prisma.courses.create({
     data: {
-      duration: 200,
-      name: "Curso de Elixir",
-      description: "Curso excelente de Elixir Rafa Camarda",
+      duration: 150,
+      name: "Curso de php",
+      description: "Curso excelente de php",
     },
   });
 
