4777f8ee9107f83f21abef49f9f5ae5f7e1e33ff
Daniele <danileaodev@gmail.com>
2022-01-24 00:34:16 -0300

    Aula10

---
+++ src/teachers/create.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const result = await prisma.teachers.create({
+    data: {
+      name: "Diego Fernandes",
+    },
+  });
+
+  console.log(result);
+}
