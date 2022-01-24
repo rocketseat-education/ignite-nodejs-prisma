9d0dca8b943c09d951951ab826161075ea765bd3
Daniele <danileaodev@gmail.com>
2022-01-24 00:01:43 -0300

    Aula09

---
+++ prisma/migrations/20220124030045_create_teacher/migration.sql
@@ -0,0 +1,23 @@
+-- CreateTable
+CREATE TABLE "modules" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "modules_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "teachers" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "teachers_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "modules_name_key" ON "modules"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "teachers_name_key" ON "teachers"("name");
