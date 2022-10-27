a2b98f6a9aaef954d4a57d1faad623d2ca219b19
Éricles Miller <ericlesmiller15@gmail.com>
2022-10-27 15:14:57 -0300

    find data

---
+++ src/teachers/findRelations.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+
+
+const prisma = new PrismaClient()
+
+
+async function main() {
+  const result = await prisma.courses.findMany({
+    include: {
+      teacher: true
+    },
+  });
+
+  console.log(result);
+}
+
+main()
