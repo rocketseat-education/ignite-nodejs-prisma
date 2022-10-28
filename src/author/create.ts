ad0365bae78c75220a9875f00035fd80423029fd
Éricles Miller <ericlesmiller15@gmail.com>
2022-10-28 15:10:01 -0300

    relations one to many

---
+++ src/author/create.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const result = await prisma.authors.create({
+    data: {
+      name: "Mayk Brito",
+      books: {
+        createMany: {
+          data: [
+            { name: "Como começar  na programação" },
+            { name: "Como ser muito produtivo" },
+          ],
+        },
+      },
+    },
+  });
+
+  console.log(result);
+}
