0485264d165db2a4fa55178f323219d120b76ddc
Éricles Miller <ericlesmiller15@gmail.com>
2022-10-28 15:15:52 -0300

    correcao

--- src/author/create.ts
+++ src/author/create.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-async function main() {
+async function create_autohor() {
   const result = await prisma.authors.create({
     data: {
       name: "Mayk Brito",
